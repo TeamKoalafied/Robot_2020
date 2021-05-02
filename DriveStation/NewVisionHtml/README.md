343512403cadf51816cb92af5db21a6a78c42a92
Lachlan Kemp <me@lachlankemp.com>
2021-05-02 11:53:53 +0930

    added new html dashboard
    and moved old to new folder

---
+++ DriveStation/NewVisionHtml/README.md
@@ -0,0 +1,22 @@
+# web-dashboard
+Please use this for viewing camera feeds and hopfully debugging..
+
+## Installing pynetworktables2js
+Make sure to install python 3 on your computer, and on Windows you can execute:
+
+```py -3 -m pip install pynetworktables2js```
+
+On Linux/OSX you can execute:
+
+```pip install pynetworktables2js```
+
+## Starting the server
+To serve the files, run 
+
+```npm start```
+or run with python
+```cd src && py -3 -m pynetworktables2js --team 6996```
+
+Any questions go to Lachlan 
+
+-- Lachlan
---
+++ DriveStation/NewVisionHtml/src/assets/app.js
@@ -0,0 +1,64 @@
+// Grab Aim Cam
+var frontCanvas = document.getElementById("front");
+var backCanvas = document.getElementById("back");
+var ctx1 = frontCanvas.getContext("2d");
+var ctx2 = backCanvas.getContext("2d");
+// Set little green line
+ctx1.fillStyle = "#04db21";
+ctx1.fillRect(backCanvas.width/2, 0, 2.5, backCanvas.height);
+ctx2.fillStyle = "#04db21";
+ctx2.fillRect(frontCanvas.width/2, 0, 2.5, frontCanvas.height);
+
+
+// 👍 documentation
+
+// Find DOM elements
+const RHardLimit = document.getElementById('RHardLimit')
+const telemStatus = document.getElementById('telemetry')
+const heading = document.getElementById('heading')
+const shooterTarget = document.getElementById('shootertarget')
+const winchPosition = document.getElementById('winchposition')
+const aimX = document.getElementById('aimx')
+const winchLimit = document.getElementById('winchlimit')
+
+
+// Show toast on connect
+NetworkTables.addRobotConnectionListener(function(connected){
+  displaytoast(`Robot Telemetry: ${connected}`, 3)
+  setSquareColour(telemStatus, 'green')
+}, true);
+
+
+/* Updaters */
+// Robot heading
+NetworkTables.addKeyListener('/SmartDashboard/Heading', (key, value) => {
+  setText(heading, value.toFixed(2))
+})
+// Shooter target rpm
+NetworkTables.addKeyListener('/SmartDashboard/Shooter Target RPM', (key, value) => {
+  setText(shooterTarget, value)
+})
+// Shooter target rpm
+NetworkTables.addKeyListener('/SmartDashboard/Winch Position Inch', (key, value) => {
+  setText(winchPosition, value)
+})
+// Vision aim x
+NetworkTables.addKeyListener('/pivision/tx', (key, value) => {
+  setText(aimX, value.toFixed(2))
+})
+NetworkTables.addKeyListener('/pivision/tx', (key, value) => {
+  if (winchLimit) {
+    setSquareColour(winchLimit, 'green')
+  } else {
+    setSquareColour(winchLimit, 'green')
+  }
+})
+
+
+/* UI Updates */
+function setSquareColour(htmlobject, colour) {
+  htmlobject.style.backgroundColor = `var(--${colour})` 
+}
+function setText(htmlobject, text) {
+  htmlobject.innerText = text
+}
